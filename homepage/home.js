55544a08e3dff9c76968f3a3bb4e2de910e6dd64
Trịnh Ngọc Lợi <loitn148@gmail.com>
2017-11-21 17:09:19 +0700

    Slideshow homepage

--- homepage/home.js
+++ homepage/home.js
@@ -1,26 +1,37 @@
+var slides = document.getElementsByClassName('slide-item');
+var dots = document.getElementsByClassName('dot');
 var slideIndex = 1;
-showSlides(slideIndex);
+showSlides();
+var show = setInterval(showSlides, 3000);
+
+function showSlides() {
+	for (var i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (var i = 0; i < dots.length; i++){
+		dots[i].className = dots[i].className.replace("active", "");
+	}
+
+	if( slideIndex > slides.length ) { slideIndex = 1 }
+	if( slideIndex < 1 ) { slideIndex = slides.length }
+
+	slides[slideIndex-1].style.display = "block";
+	dots[slideIndex-1].className += " active";
+
+	slideIndex++;
+}
 
 function plusSlides(n) {
-	showSlides(slideIndex += n);
+	slideIndex--;
+	slideIndex += n;
+	showSlides();
+	clearInterval(show);
+	show = setInterval(showSlides, 3000);
 }
 
 function currentSlide(n) {
-	showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-	var i;
-	var slides = document.getElementByClassName('slideItem');
-	var dots = document.getElementByClassName('dot');
-	if(n > slides.lenght) {slideIndex = 1;}
-	if( n < 1 ) {slideIndex = slides.lenght;}
-	for (i = 0; i < slides.lenght; i++) {
-		slides[i].style.display = "none";
-	}
-	for (i = 0; i < dots.lenght; i++){
-		dots[i].className = dots[i].className.replace("active", "");
-	}
-	slides[slideIndex-1].style.display = "block";
-	dots[slideIndex-1].className += "active";
+	slideIndex = n;
+	showSlides();
+	clearInterval(show);
+	show = setInterval(showSlides, 3000);
 }
