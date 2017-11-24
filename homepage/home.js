df5d23da75bc41a2a4c1a0fab7ea60af1b2af9a8
Trịnh Ngọc Lợi <loitn148@gmail.com>
2017-11-25 02:44:03 +0700

    homepage 90%

--- homepage/home.js
+++ homepage/home.js
@@ -35,3 +35,18 @@ function currentSlide(n) {
 	clearInterval(show);
 	show = setInterval(showSlides, 3000);
 }
+
+(function(){  $('.carousel-showmanymoveone .item').each(function(){    
+
+		var itemToClone = $(this);
+    	for (var i = 1; i < 4; i++) {      
+		 
+			itemToClone = itemToClone.next();
+			       
+			if (!itemToClone.length) {        
+				itemToClone = $(this).siblings(':first');      
+			} 
+			itemToClone.children(':first-child').clone().addClass("cloneditem-"+(i)).appendTo($(this)); 
+		}  
+	});
+}());
