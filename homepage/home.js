5bf4030c4171fc7b398e5676edf32f6080907d89
Trịnh Ngọc Lợi <loitn148@gmail.com>
2018-01-07 15:35:37 +0700

    Change logo

--- homepage/home.js
+++ homepage/home.js
@@ -1,52 +1,49 @@
 
-var slides = document.getElementsByClassName('slide-item');
-var dots = document.getElementsByClassName('dot');
-var slideIndex = 1;
-showSlides();
-var show = setInterval(showSlides, 5000);
+// var slides = document.getElementsByClassName('slide-item');
+// var dots = document.getElementsByClassName('dot');
+// var slideIndex = 1;
+// showSlides();
+// var show = setInterval(showSlides, 5000);
 
-function showSlides() {
-	for (var i = 0; i < slides.length; i++) {
-		slides[i].style.display = "none";
-	}
-	for (var i = 0; i < dots.length; i++){
-		dots[i].className = dots[i].className.replace("active", "");
-	}
+// function showSlides() {
+// 	for (var i = 0; i < slides.length; i++) {
+// 		slides[i].style.display = "none";
+// 	}
+// 	for (var i = 0; i < dots.length; i++){
+// 		dots[i].className = dots[i].className.replace("active", "");
+// 	}
 
-	if( slideIndex > slides.length ) { slideIndex = 1 }
-	if( slideIndex < 1 ) { slideIndex = slides.length }
+// 	if( slideIndex > slides.length ) { slideIndex = 1 }
+// 	if( slideIndex < 1 ) { slideIndex = slides.length }
 
-	slides[slideIndex-1].style.display = "block";
-	dots[slideIndex-1].className += " active";
+// 	slides[slideIndex-1].style.display = "block";
+// 	dots[slideIndex-1].className += " active";
 
-	slideIndex++;
-}
+// 	slideIndex++;
+// }
 
-function plusSlides(n) {
-	slideIndex--;
-	slideIndex += n;
-	showSlides();
-	clearInterval(show);
-	show = setInterval(showSlides, 5000);
-}
+// function plusSlides(n) {
+// 	slideIndex--;
+// 	slideIndex += n;
+// 	showSlides();
+// 	clearInterval(show);
+// 	show = setInterval(showSlides, 5000);
+// }
 
-function currentSlide(n) {
-	slideIndex = n;
-	showSlides();
-	clearInterval(show);
-	show = setInterval(showSlides, 5000);
-}
+// function currentSlide(n) {
+// 	slideIndex = n;
+// 	showSlides();
+// 	clearInterval(show);
+// 	show = setInterval(showSlides, 5000);
+// }
 
 $(function(){
 	$('.slideshow').hover(function(){
 		$('.slide-info-film').fadeIn('100');
 		$('.play-film').fadeIn('100');
-		clearInterval(show);
 	}, function(){
 		$('.slide-info-film').fadeOut();
 		$('.play-film').fadeOut();
-		clearInterval(show);
-		show = setInterval(showSlides, 5000);
 	})
 });
 
